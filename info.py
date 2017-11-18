cf9da072f65e8cff4b4e645c61cea8b730d4f735
Artem Lisenkov <radeon-1@mail.ru>
2017-11-18 16:43:06 +0300

    first

---
+++ info.py
@@ -0,0 +1,12 @@
+
+user_info = {}
+
+first_name_in = input('введите имя: ')
+last_name_in = input('введите имя2: ')
+
+
+user_info['first_name'] = first_name_in
+user_info['last_name'] = last_name_in
+
+print(user_info)
+
---
+++ test.py
@@ -0,0 +1,27 @@
+
+
+
+#dicts={ "name": "Маша","city": "Moscow", "temp" : "10", "wind": "восточный" }
+
+peoples={
+	'Вася': {'name': 'Vasya', 'city': 'Moscow', 'temp': '25'},
+	'Ваня': {'name': 'Vanya', 'city': 'Kzn', 'temp': '23'},
+}
+
+
+name_in = input('введите имя: ')
+print(peoples.get(name_in)["city"])
+
+
+
+
+
+
+
+#a = 1
+
+#d = [{'b': 2}]
+#d[0]['b']
+
+#input()
+
